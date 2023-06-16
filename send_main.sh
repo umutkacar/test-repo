f38ba1fb25a7463cfdef1ba6ffcc69713a63c7e7
Umut Erol Kaçar <umutkacar@gmail.com>
2023-06-16 09:59:47 +0300

    add mail sh

---
+++ send_main.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+curl -s \
+	-X POST \
+	--user "${USERNAME}:${PASSWORD}" \
+	https://api.mailjet.com/v3/send \
+	-H 'Content-Type: application/json' \
+	-d '{
+      "FromEmail":"info@wegotwe.com",
+      "FromName":"Your Mailjet Pilot",
+      "Recipients":[
+        {
+          "Email":"umutkacar@gmail.com",
+          "Name":"Umut Erol Kaçar"
+        }
+      ],
+      "Subject":"Your email flight plan!",
+      "Text-part":"Dear passenger, welcome to Mailjet! May the delivery force be with you!",
+      "Html-part":"<h3>Dear passenger, welcome to Mailjet!</h3><br />May the delivery force be with you!"
+	}'
