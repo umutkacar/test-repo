9dc278ac3bbec3db9730c6b4eadcef91e467cbb5
Umut Erol Kaçar <umutkacar@gmail.com>
2023-06-20 16:04:51 +0300

    add single quote

--- send_mail.sh
+++ send_mail.sh
@@ -20,6 +20,6 @@ curl -o curl_output.txt \
         }
       ],
       "Subject":"Your email flight plan!",
-      "Text-part":"Dear passenger, welcome to Mailjet! May the delivery force be with you! This is sent via Drone-CI with pipeline build number: ${DRONE_BUILD_NUMBER}!",
-      "Html-part":"<h3>Dear passenger, welcome to Mailjet!</h3><br />May the delivery force be with you! This is sent via Drone-CI with pipeline build number: ${DRONE_BUILD_NUMBER}!"
+      "Text-part":"Dear passenger, welcome to Mailjet! May the delivery force be with you! This is sent via Drone-CI with pipeline build number: '${DRONE_BUILD_NUMBER}'!",
+      "Html-part":"<h3>Dear passenger, welcome to Mailjet!</h3><br />May the delivery force be with you! This is sent via Drone-CI with pipeline build number: '${DRONE_BUILD_NUMBER}'!"
 	}'
